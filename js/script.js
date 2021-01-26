ffb05b5640615760162412f009aa1ed36f919bd3
Samin Yasar <yasarsamin57@gmail.com>
2021-01-26 19:00:58 +0600

    All Files Added

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// // basic setup
+
+// document.addEventListener("contextmenu", function (e) {
+//     e.preventDefault();
+//     // alert("This website's all content is protected. Can not copy anything.");
+// });
