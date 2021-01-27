e8faa411725ad0a935f6e7b31c0253717c5b56c0
Samin Yasar <yasarsamin57@gmail.com>
2021-01-27 19:15:18 +0600

    Responsive

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
+/*
+ * Title:
+ * Description:
+ * Author: Samin Yasar
+ * Date: 27/January/2021
+ * Variable name type: variableName
+ */
+
 // // basic setup
 
 // document.addEventListener("contextmenu", function (e) {
 //     e.preventDefault();
 //     // alert("This website's all content is protected. Can not copy anything.");
 // });
+
+// select all necessary elements
+
+const hamburgerIcon = document.getElementById("hamburgerIcon");
+const mobileMenu = document.getElementById("mobileMenu");
+
+// hamburger menu functionality
+hamburgerIcon.addEventListener("click", () => {
+  if (hamburgerIcon.classList.contains("fa-bars")) {
+    hamburgerIcon.classList.remove("fa-bars");
+    hamburgerIcon.classList.add("fa-times");
+    mobileMenu.classList.add("showmobileMenu");
+  } else {
+    hamburgerIcon.classList.remove("fa-times");
+    hamburgerIcon.classList.add("fa-bars");
+    mobileMenu.classList.remove("showmobileMenu");
+  }
+});
